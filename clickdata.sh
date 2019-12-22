bfde9574149a070a6e75b7618e423e1914506866
starkovad <starkovad@mail.ru>
2019-12-22 10:11:26 -0500

    fix double database creating

--- clickdata.sh
+++ clickdata.sh
@@ -4,9 +4,5 @@ set -e
 clickhouse client -n <<-EOSQL
     CREATE DATABASE oanda;
     CREATE TABLE oanda.daily_prices (x Int32) ENGINE = Log;
-EOSQL
-
-clickhouse client -n <<-EOSQL
-    CREATE DATABASE oanda;
     CREATE TABLE oanda.monthly_prices (x Int32) ENGINE = Log;
 EOSQL
